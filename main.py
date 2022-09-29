6887dbb7f1c3760d80655a6eae8336f619837ca3
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-09-29 23:23:12 +0000

    added the summary

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ print("  c. 3")
 secondanswer=input("Please enter 'a' - 'c': ")
 print( )
 
-print("~Location~")
+print("~ Location ~")
 # ask user if they want to live in a city
 print("3. Do you want to live in a city?")
-thirdanswer=input("Please enter 'yes or no': ")
+thirdanswer=input("Please enter 'yes' or 'no': ")
 print( )
 
 # ask user which state they want to live in
@@ -40,7 +40,23 @@ print("4. What state do you want to live in?")
 fourthanswer=input("Please enter the name of the state: ")
 print( )
 
+print("~ Design ~")
+# ask user what color they want they want their house to be
+print("5. What color do you want your house to be?")
+fifthanswer=input("Please enter the color you would like your house to be: ")
+print( )
 
-# features
-# How many floors do you want? 
-# do you want a garage? yes or no
+#ask the user if they want a garage
+print("6. Do you want a garage attached to your house?")
+sixthanswer=input("Please enter 'yes' or 'no': ")
+print( )
+
+# display all the answers in a summary
+print("=================")
+print("~ Summary ~")
+print(f"Number of bedrooms: {firstanswer} ")
+print(f"Number of bathrooms: {secondanswer}")
+print(f"Live in a city: {thirdanswer}")
+print(f"State to live in: {fourthanswer}")
+print(f"Color of house: {fifthanswer}")
+print(f"Garage attached: {sixthanswer}")
