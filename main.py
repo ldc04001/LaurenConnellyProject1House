73529fa8ce1ab35211744002442602b70e23818d
ldc04001 <114703505+ldc04001@users.noreply.github.com>
2022-09-29 22:55:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# create the header
+print("====================")
+print("Welcome to the UMBC")
+print("House Hunting Form!")
+print("====================")
+print()
+# provide instructions for multiple choice
+print("(For multiple choice problems, please enter the letter of the selection you're looking for)")
+print("~ Rooms~")
+
+#ask user how many bedrooms they want
+print("1. How many bedrooms do you want in your home?")
+print("  a. 1")
+print("  b. 2")
+print("  c. 3")
+print("  d. 4")
+
+#ask user to enter letter a-d
+firstanswer=input("Please enter 'a' - 'd':  ")
+
+# Rooms
+# select how many bedrooms? multiple choice
+# How many bathrooms? mult choice
+
+# Location
+# Do you want to live in the city - yes or no
+# What state do you want to live in - fill in
+
+# features
+# How many floors do you want? 
+# do you want a garage? yes or no
